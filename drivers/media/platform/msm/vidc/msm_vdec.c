11353f4e8d9c834437912e45f2320fa327c1be74
PlayfulGod <playfulgod@gmail.com>
2014-06-28 17:06:35 -0400

    msm: vidc: Query the core on VIDIOC_G_FMT Previously we only queried the core for buffer requirements in VIDIOC_REQBUFS, and relied on that information in _G_FMT.  However, there's no expectation that the client calls _REQBUFS before _G_FMT, which leads to stale information being returned as part of _G_FMT.
    As such, always retreive fresh info from the core for either ioctl.
    
    Change-Id: Ife94343f4bd4f62da7f2bef4266076ceed409bca
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vdec.c
+++ drivers/media/platform/msm/vidc/msm_vdec.c
@@ -706,6 +706,14 @@ int msm_vdec_g_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 			"Invalid input, inst = %p, format = %p\n", inst, f);
 		return -EINVAL;
 	}
+
+	rc = msm_comm_try_get_bufreqs(inst);
+ 	if (rc) {
+ 		dprintk(VIDC_ERR, "Getting buffer requirements failed: %d\n",
+ 				rc);
+ 		return rc;
+ 	}
+
 	hdev = inst->core->device;
 	if (f->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE)
 		fmt = inst->fmts[CAPTURE_PORT];
